3c16dcf3fae33947ac5e06d67849ac1599f63b2d
ebssgd <iamebssgd@yahoo.com>
2022-05-21 16:45:48 -0500

    Starting base setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import AddIssue from './components/AddIssue/AddIssue.js';
+import CurrentIssue from './components/CurrentIssue/CurrentIssue.js';
+
+import "./App.css";
 
 function App() {
+  const [addIssue, setAddIssue] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Issue Tracker</h1>
+      {addIssue ? <AddIssue /> : <CurrentIssue />}
     </div>
   );
 }
